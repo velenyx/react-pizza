2d9f4ba873e7bbef3a90d0da8ced026ac2747c10
Valenyx <dmitriysivritkin@gmail.com>
2023-06-19 22:05:58 +0300

    add(FSD): change architecture to feature sliced design

---
+++ src/app/index.tsx
@@ -0,0 +1,6 @@
+import { HomePage } from '@/pages'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<HomePage />
+)
---
+++ src/pages/ui/Page/Page.tsx
@@ -0,0 +1,3 @@
+export function HomePage() {
+	return <div>123</div>
+}
