fd0a42eb6a63bd287ffb0244e4c94414a78af0e5
Valenyx <dmitriysivritkin@gmail.com>
2023-06-25 20:02:21 +0300

    test: 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import eslint from 'vite-plugin-eslint'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      cache: false,
+      include: ['./src/**/*.js', './src/**/*.jsx'],
+      exclude: [],
+    }),
+  ],
   resolve: {
     alias: {
       '~': resolve(__dirname, 'src'),
